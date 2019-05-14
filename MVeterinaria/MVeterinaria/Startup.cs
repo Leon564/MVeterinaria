d7ca656d5448f4c8266c0b8c19d62c7b7270e282
Nelson Leon <nleon564@hotmail.com>
2019-05-13 21:06:23 -0600

    creacion

---
+++ MVeterinaria/MVeterinaria/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MVeterinaria.Startup))]
+namespace MVeterinaria
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
